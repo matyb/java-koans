ec3a0a7905ac0bfc80bdefde561f736a133a7e31
matyb <mathewkbentley@gmail.com>
2011-01-14 10:10:16 -0500

    refactoring the suite and making code testable, unit tests on the way

---
+++ src/com/sandwich/koans/suites/AllKoans.java
@@ -0,0 +1,17 @@
+package com.sandwich.koans.suites;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AllKoans {
+
+	@SuppressWarnings("unchecked")
+	private static List<Class<?>> koans = Arrays.asList(
+			AboutAssertions.class,
+			AboutAnonymousMocking.class);
+	
+	public static List<Class<?>> getKoans(){
+		return koans;
+	}
+	
+}
