c947a60fa45f8d6159d591b052b7b56096a5d6df
matyb <mathewkbentley@gmail.com>
2011-01-23 22:48:01 -0500

    AboutArrays and removal of redundant AllKoans class

--- src/com/sandwich/koans/KoanSuiteRunner.java
+++ src/com/sandwich/koans/KoanSuiteRunner.java
@@ -9,8 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import com.sandwich.koans.suites.AllKoans;
-
 class KoanSuiteRunner {
 
 	public static void main(String... args) throws IllegalArgumentException,
