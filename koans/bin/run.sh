962cb80f5096ff0e7d81766fb22b8c4a11b46a11
Mathew K Bentley <mathewkbentley@gmail.com>
2011-03-20 19:10:43 -0400

    removing bin dirs

--- koans/bin/run.sh
+++
@@ -1,5 +0,0 @@
-clear
-javac -d ../bin -classpath ../lib/koans.jar beginner/*.java
-javac -d ../bin -classpath ../lib/koans.jar intermediate/*.java
-javac -d ../bin -classpath ../lib/koans.jar advanced/*.java
-java -classpath ../bin:../lib/koans.jar com.sandwich.koan.runner.KoanSuiteRunner
