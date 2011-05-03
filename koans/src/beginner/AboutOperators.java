cab3491dc5a15fd1884ccb05597a402b50dabc57
Mike Cornell <mcornell74@gmail.com>
2011-05-03 08:54:17 -0400

    Added Bit Shifting operators

--- koans/src/beginner/AboutOperators.java
+++ koans/src/beginner/AboutOperators.java
@@ -57,4 +57,28 @@ public class AboutOperators {
 		// How could you write the condition 'with a twist' to avoid this trap?
 	}
 	
+	@Koan
+	public void aboutBitShiftingRightShift() {
+		int rightShift = 8;
+		rightShift = rightShift >> 1; 
+		assertEquals(rightShift, __);
+	}
+	
+	@Koan
+	public void aboutBitShiftingLeftShift() {
+		int leftShift = 0x80000000; // Is this number positive or negative?
+		leftShift = leftShift << 1;
+		assertEquals(leftShift, __);
+	}
+	
+	@Koan
+	public void aboutBitShiftingRightUnsigned() {
+		int rightShiftNegativeStaysNegative = 0x80000000;
+		rightShiftNegativeStaysNegative = rightShiftNegativeStaysNegative >> 4;
+		assertEquals(rightShiftNegativeStaysNegative, __);
+		int unsignedRightShift = 0x80000000; // always fills with 0
+		unsignedRightShift >>>= 4; // Just like +=
+		assertEquals(unsignedRightShift, __);
+	}
+	
 }
