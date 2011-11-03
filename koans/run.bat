61e06b6464c7e357440042a78a6ab27f0db1757e
matyb <mathewkbentley@gmail.com>
2011-11-03 14:53:05 -0400

    execute koans outside koans/ dir

--- koans/run.bat
+++ koans/run.bat
@@ -6,8 +6,8 @@ if ERRORLEVEL 3 goto no_javac
 java -version
 if ERRORLEVEL 1 goto no_java
 cls
-java -classpath bin;lib\koans.jar com.sandwich.koan.runner.AppLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
- 
+java -classpath "%~dp0\bin;%~dp0\lib\koans.jar" com.sandwich.koan.runner.AppLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
+
 goto end
 
 :no_java
