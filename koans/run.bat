4b96c6a148dd75c923570723f59414f6cfcbe8ce
matyb <mathewkbentley@gmail.com>
2011-11-02 23:17:22 -0400

    externalize lesson definitions from xml

---
+++ koans/run.bat
@@ -0,0 +1,24 @@
+@echo off
+cls
+
+javac -version
+if ERRORLEVEL 3 goto no_javac
+java -version
+if ERRORLEVEL 1 goto no_java
+mkdir ..\bin
+cls
+java -classpath bin;lib\koans.jar com.sandwich.koan.runner.AppLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
+ 
+goto end
+
+:no_java
+REM cls
+@echo java is not bound to PATH variable.
+goto end
+
+:no_javac
+REM cls
+@echo javac is not bound to PATH variable.
+goto end
+
+:end
---
+++ koans/run.sh
@@ -0,0 +1,3 @@
+mkdir ../bin
+clear
+java -classpath bin:lib/koans.jar com.sandwich.koan.runner.AppLauncher "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
