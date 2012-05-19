83c81c941eff60a1b83bb1f93a463c3847ac2cbf
Mat Bentley <mathewkbentley@gmail.com>
2012-05-19 02:57:18 -0400

    better handling of absence of java/javac on PATH in *nix

--- koans/run.sh
+++ koans/run.sh
@@ -1,4 +1,15 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir "$DIR"/bin
+function exitOnError()
+{
+	rc=$?
+	if [[ $rc != 0 ]] ; then
+	    exit $rc
+	fi
+}
+javac -help
+exitOnError
+java -version
+exitOnError
 clear
 java -classpath "$DIR"/bin:"$DIR"/lib/koans.jar com.sandwich.koan.runner.AppLauncher "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
