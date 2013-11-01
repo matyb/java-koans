d1e3e1c24b1d4d078730a634c5aab7f7705f2ea2
Jacob Tomaw <JACOB.TOMAW@orbitz.com>
2013-11-01 09:20:22 -0500

    Made run quieter and built classpath to include all jars in lib

--- koans/run.sh
+++ koans/run.sh
@@ -1,6 +1,6 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir "$DIR"/app
-mkdir "$DIR"/app/bin
+mkdir -p "$DIR"/app
+mkdir -p "$DIR"/app/bin
 function exitOnError()
 {
 	rc=$?
@@ -8,9 +8,19 @@ function exitOnError()
 	    exit $rc
 	fi
 }
-javac -help
+
+function buildClasspath()
+{
+    appDir=$1
+    classpath=$appDir/bin
+
+    for jar in $appDir/lib/*; do
+        classpath=$classpath:$jar
+    done
+}
+javac -help > /dev/null 2>&1
 exitOnError
-java -version
+java -version > /dev/null 2>&1
 exitOnError
-clear
-java -classpath "$DIR"/app/bin:"$DIR"/app/lib/koans.jar com.sandwich.koan.runner.AppLauncher "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+buildClasspath "$DIR"/app
+java -classpath $classpath com.sandwich.koan.runner.AppLauncher "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
