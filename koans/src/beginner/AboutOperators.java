19968c4e696f69a7e506da8e45321d9fee2d7419
MichaelThessel <der.thes@gmail.com>
2013-12-15 19:08:25 -1000

    Whitespace cleanup

--- koans/src/beginner/AboutOperators.java
+++ koans/src/beginner/AboutOperators.java
@@ -9,10 +9,10 @@ public class AboutOperators {
 	@Koan
 	public void plusPlusVariablePlusPlus(){
 		int i = 1;
-		assertEquals(++i,__);
-		assertEquals(i,__);
-		assertEquals(i++,__);
-		assertEquals(i,__);
+		assertEquals(++i, __);
+		assertEquals(i, __);
+		assertEquals(i++, __);
+		assertEquals(i, __);
 	}
 	
 	@Koan
@@ -21,22 +21,22 @@ public class AboutOperators {
 		int a = 6; // Why did we use a variable here?
 		// What happens if you replace 'a' with '6' below?
 		// Try this with an IDE like Eclipse...
-		if ( (a < 9 ) || (++i < 8)  )  i = i + 1;
-		assertEquals(i,__);
+		if ( (a < 9 ) || (++i < 8) )  i = i + 1;
+		assertEquals(i, __);
 	}
 	
 	@Koan
 	public void fullAnd(){
 		int i = 1;
-		if ( true & (++i < 8)  )  i = i + 1;
-		assertEquals(i,__);
+		if ( true & (++i < 8) )  i = i + 1;
+		assertEquals(i, __);
 	}
 	
 	@Koan
 	public void shortCircuitAnd(){
 		int i = 1;
-		if ( true && (i < -28)  )  i = i + 1;
-		assertEquals(i,__);
+		if ( true && (i < -28) )  i = i + 1;
+		assertEquals(i, __);
 	}
 	
 	@Koan
@@ -44,16 +44,16 @@ public class AboutOperators {
 		int i = 1;
 		int a = 6;
 		if ( (a < 9 ) ^ false)  i = i + 1;
-		assertEquals(i,__);
+		assertEquals(i, __);
 	}
 	
 	@Koan
 	public void dontMistakeEqualsForEqualsEquals() {
 		int i = 1;
 		boolean a = false;
-		if (a = true)  i++;
+		if (a = true) i++;
 		assertEquals(a, __);
-		assertEquals(i,__);
+		assertEquals(i, __);
 		// How could you write the condition 'with a twist' to avoid this trap?
 	}
 	
