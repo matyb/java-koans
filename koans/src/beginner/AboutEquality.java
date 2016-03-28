a44f94e096c93e56a529800aba964a77a26e24e4
Maciej Falski <takaczapka@gmail.com>
2016-03-28 10:34:33 +0100

    a few assertions on object equality added

--- koans/src/beginner/AboutEquality.java
+++ koans/src/beginner/AboutEquality.java
@@ -28,16 +28,25 @@ public class AboutEquality {
         assertEquals(object.equals(object), __);
         assertEquals(object.equals(new Integer(1)), __);
         // Note: This means that for the class 'Object' there is no difference between 'equal' and 'same'
+        // but for the class 'Integer' there is difference - see below
+    }
+
+    @Koan
+    public void equalsMethodCanBeChangedBySubclassesToTestsIfTwoObjectsAreEqualExample() {
+        Integer value1 = new Integer(4);
+        Integer value2 = new Integer(2 + 2);
+        assertEquals(value1.equals(value2), __);
+        assertEquals(value1, __);
     }
 
     @Koan
     public void objectsNeverEqualNull() {
-        assertEquals(new Object().equals(null), __);
+        assertEquals(new Object().equals(null), false);
     }
 
     @Koan
     public void objectsEqualThemselves() {
         Object obj = new Object();
-        assertEquals(obj.equals(obj), __);
+        assertEquals(obj.equals(obj), true);
     }
 }
