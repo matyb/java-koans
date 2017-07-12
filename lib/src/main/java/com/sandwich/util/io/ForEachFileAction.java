3cbe8fcdf585096030de3cd6eff3b24838327925
matyb <mathewkbentley@gmail.com>
2017-07-12 00:10:52 -0400

    This closes #72 - ignore idea's temporary files

--- lib/src/main/java/com/sandwich/util/io/ForEachFileAction.java
+++ lib/src/main/java/com/sandwich/util/io/ForEachFileAction.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 
 public abstract class ForEachFileAction extends ExistingFileAction {
 
+	public ForEachFileAction(String... strings) {
+		super(strings);
+	}
+
 	public void onDirectory(File dir) throws IOException {
 		for (String fileName : dir.list()) {
 			operate(new File(dir, fileName));
