20a64c4e8b32bfd009ce49b0123e4d531a98ab0a
AboutEquality finished <@no>
2017-10-04 17:12:43 -0700

    Did the do and finished questions or something

--- koans/src/beginner/AboutStrings.java
+++ koans/src/beginner/AboutStrings.java
@@ -12,7 +12,7 @@ public class AboutStrings {
 
     @Koan
     public void implicitStrings() {
-        assertEquals("just a plain ole string".getClass(), __);
+        assertEquals("just a plain ole string".getClass(), String.class);
     }
 
     @Koan
@@ -21,47 +21,47 @@ public class AboutStrings {
         // practice - generally it is redundant, and done repetitively can be slow
         String string = new String();
         String empty = "";
-        assertEquals(string.equals(empty), __);
+        assertEquals(string.equals(empty), true);
     }
 
     @Koan
     public void newStringIsRedundant() {
         String stringInstance = "zero";
         String stringReference = new String(stringInstance);
-        assertEquals(stringInstance.equals(stringReference), __);
+        assertEquals(stringInstance.equals(stringReference), true);
     }
 
     @Koan
     public void newStringIsNotIdentical() {
         String stringInstance = "zero";
         String stringReference = new String(stringInstance);
-        assertEquals(stringInstance == stringReference, __);
+        assertEquals(stringInstance == stringReference, false);
     }
 
     @Koan
     public void stringIsEmpty() {
-        assertEquals("".isEmpty(), __);
-        assertEquals("one".isEmpty(), __);
-        assertEquals(new String().isEmpty(), __);
-        assertEquals(new String("").isEmpty(), __);
-        assertEquals(new String("one").isEmpty(), __);
+        assertEquals("".isEmpty(), true);
+        assertEquals("one".isEmpty(), false);
+        assertEquals(new String().isEmpty(), true);
+        assertEquals(new String("").isEmpty(), true);
+        assertEquals(new String("one").isEmpty(), false);
     }
 
     @Koan
     public void stringLength() {
-        assertEquals("".length(), __);
-        assertEquals("one".length(), __);
-        assertEquals("the number is one".length(), __);
+        assertEquals("".length(), 0);
+        assertEquals("one".length(), 3);
+        assertEquals("the number is one".length(), 17);
     }
 
     @Koan
     public void stringTrim() {
-        assertEquals("".trim(), __);
+        assertEquals("".trim(), "");
         assertEquals("one".trim(), "one");
-        assertEquals(" one more time".trim(), __);
-        assertEquals(" one more time         ".trim(), __);
-        assertEquals(" and again\t".trim(), __);
-        assertEquals("\t\t\twhat about now?\t".trim(), __);
+        assertEquals(" one more time".trim(), "one more time");
+        assertEquals(" one more time         ".trim(), "one more time");
+        assertEquals(" and again\t".trim(), "and again");
+        assertEquals("\t\t\twhat about now?\t".trim(), "what about now?");
     }
 
     @Koan
@@ -69,8 +69,8 @@ public class AboutStrings {
         String one = "one";
         String space = " ";
         String two = "two";
-        assertEquals(one + space + two, __);
-        assertEquals(space + one + two, __);
+        assertEquals(one + space + two, "one two");
+        assertEquals(space + one + two, " onetwo");
         assertEquals(two + space + one, __);
     }
 
