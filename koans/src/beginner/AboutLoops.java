5f354b6bfe3ab5011ef7354112a096e11dc278a6
AboutEquality finished <@no>
2017-10-04 17:47:57 -0700

    did stuff, eclipse Broke pls fix

--- koans/src/beginner/AboutLoops.java
+++ koans/src/beginner/AboutLoops.java
@@ -15,7 +15,7 @@ public class AboutLoops {
         for (int i = 0; i < 5; i++) {
             s += i + " ";
         }
-        assertEquals(s, __);
+        assertEquals(s, "0 1 2 3 4 ");
     }
 
     @Koan
@@ -24,7 +24,7 @@ public class AboutLoops {
         for (int i = -5; i < 1; i++) {
             s += i + " ";
         }
-        assertEquals(s, __);
+        assertEquals(s, "-5 -4 -3 -2 -1 0 ");
     }
 
     @Koan
@@ -33,7 +33,7 @@ public class AboutLoops {
         for (int i = 5; i > 0; i--) {
             s += i + " ";
         }
-        assertEquals(s, __);
+        assertEquals(s, "5 4 3 2 1 ");
     }
 
     @Koan
@@ -42,7 +42,7 @@ public class AboutLoops {
         for (int i = 0; i < 11; i += 2) {
             s += i + " ";
         }
-        assertEquals(s, __);
+        assertEquals(s, "0 2 4 6 8 10 ");
     }
 
     @Koan
@@ -51,7 +51,7 @@ public class AboutLoops {
         for (int i = 1; i <= 16; i *= 2) {
             s += i + " ";
         }
-        assertEquals(s, __);
+        assertEquals(s, "1 2 4 8 16 ");
     }
 
     @Koan
@@ -60,7 +60,7 @@ public class AboutLoops {
         for (int i = 0, j = 10; i < 5 && j > 5; i++, j--) {
             s += i + " " + j + " ";
         }
-        assertEquals(s, __);
+        assertEquals(s, "0 10 1 9 2 8 3 7 4 6 ");
     }
 
     @Koan
@@ -72,7 +72,7 @@ public class AboutLoops {
             }
             s += " - ";
         }
-        assertEquals(s, __);
+        assertEquals(s, "(0, 0) (0, 1) (0, 2)  - (1, 0) (1, 1) (1, 2)  - (2, 0) (2, 1) (2, 2)");
     }
 
     @Koan
