46eba583626a0e7cb62b2f9d5ab7e92ae82231d5
SD64\robotics18 <just@no>
2017-10-04 17:54:24 -0700

    finsihed up strings

--- koans/src/beginner/AboutStrings.java
+++ koans/src/beginner/AboutStrings.java
@@ -106,56 +106,56 @@ public class AboutStrings {
     public void stringStartsWith() {
         assertEquals("".startsWith("one"), false);
         assertEquals("one".startsWith("one"), true);
-        assertEquals("one is the number".startsWith("one"), __);
-        assertEquals("ONE is the number".startsWith("one"), __);
+        assertEquals("one is the number".startsWith("one"), true);
+        assertEquals("ONE is the number".startsWith("one"), false);
     }
 
     @Koan
     public void stringEndsWith() {
-        assertEquals("".endsWith("one"), __);
-        assertEquals("one".endsWith("one"), __);
-        assertEquals("the number is one".endsWith("one"), __);
-        assertEquals("the number is two".endsWith("one"), __);
-        assertEquals("the number is One".endsWith("one"), __);
+        assertEquals("".endsWith("one"),false);
+        assertEquals("one".endsWith("one"), true);
+        assertEquals("the number is one".endsWith("one"), true);
+        assertEquals("the number is two".endsWith("one"), false);
+        assertEquals("the number is One".endsWith("one"), false);
     }
 
     @Koan
     public void stringSubstring() {
         String str = "I AM a number ONE!";
-        assertEquals(str.substring(0), __);
-        assertEquals(str.substring(1), __);
-        assertEquals(str.substring(5), __);
-        assertEquals(str.substring(14, 17), __);
-        assertEquals(str.substring(7, str.length()), __);
+        assertEquals(str.substring(0), "I AM a number ONE!");
+        assertEquals(str.substring(1), " AM a number ONE!");
+        assertEquals(str.substring(5), "a number ONE!");
+        assertEquals(str.substring(14, 17),"ONE");
+        assertEquals(str.substring(7, str.length()), "number ONE!");
     }
 
     @Koan
     public void stringContains() {
         String str = "I AM a number ONE!";
-        assertEquals(str.contains("one"), __);
-        assertEquals(str.contains("ONE"), __);
+        assertEquals(str.contains("one"), false);
+        assertEquals(str.contains("ONE"), true);
     }
 
     @Koan
     public void stringReplace() {
         String str = "I am a number ONE!";
-        assertEquals(str.replace("ONE", "TWO"), __);
-        assertEquals(str.replace("I am", "She is"),  __);
+        assertEquals(str.replace("ONE", "TWO"), "I am a number TWO!");
+        assertEquals(str.replace("I am", "She is"),  "She is a number ONE!");
     }
 
     @Koan
     public void stringBuilderCanActAsAMutableString() {
-        assertEquals(new StringBuilder("one").append(" ").append("two").toString(), __);
+        assertEquals(new StringBuilder("one").append(" ").append("two").toString(),"one two");
     }
 
     @Koan
     public void readableStringFormattingWithStringFormat() {
-        assertEquals(String.format("%s %s %s", "a", "b", "a"), __);
+        assertEquals(String.format("%s %s %s", "a", "b", "a"), "a b a");
     }
 
     @Koan
     public void extraArgumentsToStringFormatGetIgnored() {
-        assertEquals(String.format("%s %s %s", "a", "b", "c", "d"), __);
+        assertEquals(String.format("%s %s %s", "a", "b", "c", "d"), "a b c");
     }
 
     @Koan
@@ -164,23 +164,23 @@ public class AboutStrings {
             String.format("%s %s %s", "a", "b");
             fail("No Exception was thrown!");
         } catch (Exception e) {
-            assertEquals(e.getClass(), __);
-            assertEquals(e.getMessage(), __);
+            assertEquals(e.getClass(), java.util.MissingFormatArgumentException.class);
+            assertEquals(e.getMessage(), "Format specifier '%s'");
         }
     }
 
     @Koan
     public void readableStringFormattingWithMessageFormat() {
-        assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b"), __);
+        assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b"), "a b a");
     }
 
     @Koan
     public void extraArgumentsToMessageFormatGetIgnored() {
-        assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b", "c"), __);
+        assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b", "c"), "a b a");
     }
 
     @Koan
     public void insufficientArgumentsToMessageFormatDoesNotReplaceTheToken() {
-        assertEquals(MessageFormat.format("{0} {1} {0}", "a"), __);
+        assertEquals(MessageFormat.format("{0} {1} {0}", "a"), "a {1} a");
     }
 }
