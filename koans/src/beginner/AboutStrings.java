4e2e8d7e1f1a0393193b6cf29290f93fa6035b51
AboutEquality finished <@no>
2017-10-04 17:37:34 -0700

    Diddly done progressed

--- koans/src/beginner/AboutStrings.java
+++ koans/src/beginner/AboutStrings.java
@@ -164,23 +164,23 @@ public class AboutStrings {
             String.format("%s %s %s", "a", "b");
             fail("No Exception was thrown!");
         } catch (Exception e) {
-            assertEquals(e.getClass(), __);
-            assertEquals(e.getMessage(), __);
+            assertEquals(e.getClass(), java.util.MissingFormatArgumentException.class);
+            assertEquals(e.getMessage(), "Format specifier '%s'");
         }
     }
 
     @Koan
     public void readableStringFormattingWithMessageFormat() {
-        assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b"), __);
+        assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b"), "a b a");
     }
 
     @Koan
     public void extraArgumentsToMessageFormatGetIgnored() {
-        assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b", "c"), __);
+        assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b", "c"), "a b a");
     }
 
     @Koan
     public void insufficientArgumentsToMessageFormatDoesNotReplaceTheToken() {
-        assertEquals(MessageFormat.format("{0} {1} {0}", "a"), __);
+        assertEquals(MessageFormat.format("{0} {1} {0}", "a"), "a {1} a");
     }
 }
