475d0beebc621edbf44a4bc4fb60730b5677e22b
Javier Holguera <javier.holguera@gmail.com>
2018-02-26 20:24:25 +0000

    Beginner: arrays, bitwise operators, constructors, enums, exceptions, method preference and varargs

--- koans/src/beginner/AboutBitwiseOperators.java
+++ koans/src/beginner/AboutBitwiseOperators.java
@@ -11,14 +11,14 @@ public class AboutBitwiseOperators {
     public void fullAnd() {
         int i = 1;
         if (true & (++i < 8)) i = i + 1;
-        assertEquals(i, __);
+        assertEquals(i, 3);
     }
 
     @Koan
     public void shortCircuitAnd() {
         int i = 1;
         if (true && (i < -28)) i = i + 1;
-        assertEquals(i, __);
+        assertEquals(i, 1);
     }
 
     @Koan
@@ -26,7 +26,7 @@ public class AboutBitwiseOperators {
         int i = 1;
         int a = 6;
         if ((a < 9) ^ false) i = i + 1;
-        assertEquals(i, __);
+        assertEquals(i, 2);
     }
 
     @Koan
@@ -34,32 +34,33 @@ public class AboutBitwiseOperators {
         int i = 1;
         boolean a = false;
         if (a = true) i++;
-        assertEquals(a, __);
-        assertEquals(i, __);
+        assertEquals(a, true);
+        assertEquals(i, 2);
         // How could you write the condition 'with a twist' to avoid this trap?
+        // ANSWER: invert the condition and do true = a. It will not compile.
     }
 
     @Koan
     public void aboutBitShiftingRightShift() {
         int rightShift = 8;
         rightShift = rightShift >> 1;
-        assertEquals(rightShift, __);
+        assertEquals(rightShift, 4);
     }
 
     @Koan
     public void aboutBitShiftingLeftShift() {
         int leftShift = 0x80000000; // Is this number positive or negative?
         leftShift = leftShift << 1;
-        assertEquals(leftShift, __);
+        assertEquals(leftShift, 0);
     }
 
     @Koan
     public void aboutBitShiftingRightUnsigned() {
         int rightShiftNegativeStaysNegative = 0x80000000;
         rightShiftNegativeStaysNegative = rightShiftNegativeStaysNegative >> 4;
-        assertEquals(rightShiftNegativeStaysNegative, __);
+        assertEquals(rightShiftNegativeStaysNegative, -134217728);
         int unsignedRightShift = 0x80000000; // always fills with 0
         unsignedRightShift >>>= 4; // Just like +=
-        assertEquals(unsignedRightShift, __);
+        assertEquals(unsignedRightShift, 134217728);
     }
 }
