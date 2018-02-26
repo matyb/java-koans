f049e6eaea518f17a9a885af62bb9da0800fe4ed
Javier Holguera <javier.holguera@gmail.com>
2018-02-26 23:17:52 +0000

    Intermediate: autoboxing, collections and comparison

--- koans/src/intermediate/AboutComparison.java
+++ koans/src/intermediate/AboutComparison.java
@@ -14,9 +14,9 @@ public class AboutComparison {
     public void compareObjects() {
         String a = "abc";
         String b = "bcd";
-        assertEquals(a.compareTo(b), __);
-        assertEquals(a.compareTo(a), __);
-        assertEquals(b.compareTo(a), __);
+        assertEquals(a.compareTo(b), -1);
+        assertEquals(a.compareTo(a), 0);
+        assertEquals(b.compareTo(a), 1);
     }
 
     static class Car implements Comparable<Car> {
@@ -36,7 +36,7 @@ public class AboutComparison {
         vwbeetle.horsepower = 50;
         Car porsche = new Car();
         porsche.horsepower = 300;
-        assertEquals(vwbeetle.compareTo(porsche), __);
+        assertEquals(vwbeetle.compareTo(porsche), -250);
     }
 
     static class RaceHorse {
@@ -76,8 +76,8 @@ public class AboutComparison {
         RaceHorse[] horses = {lindy, slowy, lightning};
 
         Arrays.sort(horses, new HorseAgeComparator());
-        assertEquals(horses[0], __);
+        assertEquals(horses[0], lightning);
         Arrays.sort(horses, new HorseSpeedComparator());
-        assertEquals(horses[0], __);
+        assertEquals(horses[0], slowy);
     }
 }
